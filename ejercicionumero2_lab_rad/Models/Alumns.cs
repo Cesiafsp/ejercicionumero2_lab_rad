7698c3dfaf8895ccbcfa272b64f47660e112dfcd
cesia <cesia@LAPTOP-KRK3HSI1>
2023-02-28 01:54:35 -0600

    Agregar archivos de proyecto.

---
+++ ejercicionumero2_lab_rad/Models/Alumns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicionumero2_lab_rad.Models
+{
+    internal class alumno
+    {
+        int id { get; set; }
+
+        public string numero1 { get; set; }
+
+        public string numero2 { get; set; }
+
+     
+    }
+
+}
---
+++ ejercicionumero2_lab_rad/Views/PageAlum.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ejercicionumero2_lab_rad.Views
+
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageAlums : ContentPage
+    {
+        public PageAlums()
+        {
+            InitializationFlags();
+
+        }
+        private async void btnmostrar_Clicked(object sender, EventArgs e)
+        {
+            var alum = new Models.alumno
+            {
+                numero1 = txtNumerouno.Text,
+                numero2 = txtNumerodos.Text,
+               
+
+            };
+
+            var page = new Views.PageResultado();
+            page.BindingContext = alum;
+            await Navigation.PushAsync(page);
+
+
+        }
+    }
+}
